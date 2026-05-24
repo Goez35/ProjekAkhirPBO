850c6ad98e5a78734c78e4ea53597211998052d7
Goez35 <bagusrx765@gmail.com>
2026-05-24 18:56:28 +0700

    b

--- src/testprojek/Testprojek.java
+++ src/testprojek/Testprojek.java
@@ -16,6 +16,7 @@ public class Testprojek {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("tst");
+        System.out.println("tst");
     }
     
 }
